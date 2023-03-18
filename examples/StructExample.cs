20a6a680586fc402bb9abc38f362697d25c65ba2
Halen84 <55899402+Halen84@users.noreply.github.com>
2023-03-18 14:29:11 -0500

    Update StructExample.cs

--- examples/StructExample.cs
+++ examples/StructExample.cs
@@ -6,17 +6,6 @@ using RDR2.UI;          // UI related stuff
 using RDR2.Native;      // RDR2 native functions (commands)
 using RDR2.Math;        // Vectors, Quaternions, Matrixes
 
-
-// C# type aliasing is so shit...
-
-// In C#, a string is a managed type so we have to do some bullshittery to get it to work.
-// We set string values as a int64 to get around this.
-using StructString = System.Int64;
-
-// Since a C# boolean is not interpreted as 1 and 0, make it an int
-using StructBool = System.Int32;
-
-
 namespace RDR2
 {
 	public class StructExample : Script
@@ -27,16 +16,23 @@ namespace RDR2
 		}
 
 
-		[StructLayout(LayoutKind.Explicit, Size = 0x38/*56*/), Serializable]
+		[StructLayout(LayoutKind.Explicit, Size = 0x40), Serializable]
 		public struct ScriptedSpeechParams
 		{
-			[FieldOffset(0)] public StructString speechName;
-			[FieldOffset(8)] public StructString voiceName;
-			[FieldOffset(16)] public int variation;
-			[FieldOffset(24)] public uint speechParamHash;
-			[FieldOffset(32)] public int listenerPed;
-			[FieldOffset(40)] public StructBool syncOverNetwork;
-			[FieldOffset(48)] public int v7; // Always 1 (Only 0 in one game script)
+			// In C#, "string" is a managed type so we have to do some bullshittery to get this to work.
+			// We set string values (const char*) as a Int64 to get around this.
+			[FieldOffset(0x0)] public long speechName; // const char*
+			[FieldOffset(0x8)] public long voiceName; // const char*
+
+			[FieldOffset(0x10)] public int variation;
+			[FieldOffset(0x18)] public uint speechParamHash;
+			[FieldOffset(0x20)] public int listenerPed;
+
+			// Since a C# boolean is not interpreted as 1 and 0, make the type an int
+			[FieldOffset(0x28)] public int syncOverNetwork; // BOOL
+
+			[FieldOffset(0x30)] public int v7; // Always 1
+			[FieldOffset(0x38)] public int v8; // Unused in the scripts, exists in IDA
 		}
 
 		private void OnTick(object sender, EventArgs e)
@@ -47,7 +43,7 @@ namespace RDR2
 			}
 		}
 
-		private unsafe void PlaySpeechOnPed(Ped speaker, string speechName, string voiceName, uint speechParamHash)
+		private void PlaySpeechOnPed(Ped speaker, string speechName, string voiceName, uint speechParamHash)
 		{
 			ScriptedSpeechParams @params = new ScriptedSpeechParams();
 
@@ -55,23 +51,23 @@ namespace RDR2
 			IntPtr pSpeechName = MarshalManagedStrToNative(speechName);
 			IntPtr pVoiceName = MarshalManagedStrToNative(voiceName);
 
-			@params.speechName = pSpeechName.ToInt64(); // Convert the pointers to int64 (StructString)
+			@params.speechName = pSpeechName.ToInt64(); // Convert the pointers to Int64 (they will act as strings)
 			@params.voiceName = pVoiceName.ToInt64();
 			@params.variation = 1;
 			@params.speechParamHash = speechParamHash;
 			@params.listenerPed = 0;
 			@params.syncOverNetwork = 1; // TRUE
 			@params.v7 = 1;
+			@params.v8 = 0;
 
-			// RAGE Script structs must be passed as type "ulong*".
-			// So cast it to a pointer and get its address.
-			AUDIO.PLAY_PED_AMBIENT_SPEECH_NATIVE(speaker.Handle, (ulong*)&@params);
+			// RAGE Script structs must be passed to natives as type ulong* aka Any*
+			unsafe { AUDIO.PLAY_PED_AMBIENT_SPEECH_NATIVE(speaker.Handle, (ulong*)&@params); }
 
 			Marshal.FreeCoTaskMem(pSpeechName);
 			Marshal.FreeCoTaskMem(pVoiceName);
 		}
 
-		// Used for struct fields that are strings (StructString)
+		// Used for struct fields that are of type  const char*
 		private IntPtr MarshalManagedStrToNative(string str)
 		{
 			var bytes = Encoding.UTF8.GetBytes(str);
