403f1814a8684cfcf1313d4aa54efd7ab5fca892
Halen84 <55899402+Halen84@users.noreply.github.com>
2023-06-23 14:56:01 -0500

    Rewrite RDR2.Global methods
    Removed all functions in favor of newly created generic functions Set<T>(T value) and T As<T>()

--- source/scripting_v3/RDR2/Global.cs
+++ source/scripting_v3/RDR2/Global.cs
@@ -21,63 +21,100 @@ namespace RDR2
 
 		public unsafe ulong* MemoryAddress => (ulong*)address.ToPointer();
 
-		/*public unsafe void Set<T>(T value)
+		public unsafe void Set<T>(T value)
 		{
-			if (typeof(T) == typeof(int)) {
-				RDR2DN.NativeMemory.WriteInt32(address, Convert.ToInt32(value));
+			if (typeof(T) == typeof(bool))
+			{
+				if (Convert.ToBoolean(value) == false)
+				{
+					RDR2DN.NativeMemory.WriteInt32(address, 0);
+				}
+				else
+				{
+					RDR2DN.NativeMemory.WriteInt32(address, 1);
+				}
 			}
-			else if (typeof(T) == typeof(float)) {
-				RDR2DN.NativeMemory.WriteFloat(address, (float)Convert.ToDouble(value));
+			else if (typeof(T) == typeof(byte) || typeof(T) == typeof(sbyte))
+			{
+				RDR2DN.NativeMemory.WriteByte(address, Convert.ToByte(value));
 			}
-			else if (typeof(T) == typeof(string)) {
+			else if (typeof(T) == typeof(short) || typeof(T) == typeof(ushort))
+			{
+				RDR2DN.NativeMemory.WriteInt16(address, Convert.ToInt16(value));
+			}
+			else if (typeof(T) == typeof(float) || typeof(T) == typeof(double))
+			{
+				RDR2DN.NativeMemory.WriteFloat(address, Convert.ToSingle(value));
+			}
+			else if (typeof(T) == typeof(string))
+			{
 				string _value = Convert.ToString(value);
 				int size = Encoding.UTF8.GetByteCount(_value);
 				Marshal.Copy(Encoding.UTF8.GetBytes(_value), 0, address, size);
 				*((byte*)MemoryAddress + size) = 0;
 			}
-			else if (typeof(T) == typeof(Vector3)) {
-				Vector3 vec = (Vector3)Convert.ChangeType(value, typeof(Vector3));
+			else if (typeof(T) == typeof(Vector3))
+			{
+				Vector3 vec = (Vector3)(object)value;
 				RDR2DN.NativeMemory.WriteVector3(address, vec.ToArray());
 			}
-
-			throw new InvalidCastException("Cannot set type '" + value.GetType() + "' to global");
-		}*/
-
-		public unsafe void SetInt(int value)
-		{
-			RDR2DN.NativeMemory.WriteInt32(address, value);
-		}
-		public unsafe void SetFloat(float value)
-		{
-			RDR2DN.NativeMemory.WriteFloat(address, value);
-		}
-		public unsafe void SetString(string value)
-		{
-			int size = Encoding.UTF8.GetByteCount(value);
-			Marshal.Copy(Encoding.UTF8.GetBytes(value), 0, address, size);
-			*((byte*)MemoryAddress + size) = 0;
-		}
-		public unsafe void SetVector3(Vector3 value)
-		{
-			RDR2DN.NativeMemory.WriteVector3(address, value.ToArray());
+			else if (typeof(T).IsPrimitive || typeof(T).IsEnum)
+			{
+				RDR2DN.NativeMemory.WriteInt32(address, Convert.ToInt32(value));
+			}
+			else
+			{
+				throw new InvalidCastException("Cannot cast script global to type " + typeof(T).ToString() + "; not a supported cast.");
+			}
 		}
 
-		public unsafe int GetInt()
+
+		public unsafe T As<T>()
 		{
-			return RDR2DN.NativeMemory.ReadInt32(address);
-		}
-		public unsafe float GetFloat()
-		{
-			return RDR2DN.NativeMemory.ReadFloat(address);
-		}
-		public unsafe string GetString()
-		{
-			return RDR2DN.NativeMemory.PtrToStringUTF8(address);
-		}
-		public unsafe Vector3 GetVector3()
-		{
-			var data = RDR2DN.NativeMemory.ReadVector3(address);
-			return new Vector3(data[0], data[1], data[2]);
+			if (typeof(T) == typeof(bool))
+			{
+				if (RDR2DN.NativeMemory.ReadInt32(address) == 0)
+				{
+					return (T)(object)false;
+				}
+				else
+				{
+					return (T)(object)true;
+				}
+			}
+
+			if (typeof(T) == typeof(byte))
+			{
+				return (T)(object)RDR2DN.NativeMemory.ReadByte(address);
+			}
+
+			if (typeof(T) == typeof(short))
+			{
+				return (T)(object)RDR2DN.NativeMemory.ReadInt16(address);
+			}
+
+			if (typeof(T) == typeof(float))
+			{
+				return (T)(object)RDR2DN.NativeMemory.ReadFloat(address);
+			}
+
+			if (typeof(T) == typeof(string))
+			{
+				return (T)(object)RDR2DN.NativeMemory.PtrToStringUTF8(address);
+			}
+
+			if (typeof(T) == typeof(Vector3))
+			{
+				var data = RDR2DN.NativeMemory.ReadVector3(address);
+				return (T)(object)new Vector3(data[0], data[1], data[2]);
+			}
+
+			if (typeof(T).IsPrimitive || typeof(T).IsEnum)
+			{
+				return (T)(object)RDR2DN.NativeMemory.ReadInt32(address);
+			}
+
+			throw new InvalidCastException("Cannot cast script global to type " + typeof(T).ToString() + "; not a supported cast.");
 		}
 	}
 }
