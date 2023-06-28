37eede164cde610f132e253475d8e6acca113e60
Halen84 <55899402+Halen84@users.noreply.github.com>
2023-06-27 21:45:04 -0500

    Create ScriptAttributes

---
+++ source/scripting_v3/RDR2/ScriptAttributes.cs
@@ -0,0 +1,18 @@
+﻿//
+// Copyright (C) 2015 crosire & contributors
+// License: https://github.com/crosire/scripthookvdotnet#license
+//
+
+using System;
+
+namespace RDR2
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+	public sealed class ScriptAttributes : Attribute
+	{
+		public string Author;
+		public string SupportURL;
+		public bool NoScriptThread;
+		public bool NoDefaultInstance;
+	}
+}
